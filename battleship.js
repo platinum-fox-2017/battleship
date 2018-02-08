10b5ad8fb34a74f62641ab554fc5967ea324e331
ekifakhrureza <ekifakhrureza@gmail.com>
2018-02-08 23:02:15 +0700

    release 0

--- battleship.js
+++ battleship.js
@@ -1,1 +1,36 @@
-// Your code here
+"use strict"
+
+class BattleShip{
+  constructor(ship) {
+    this.ship=ship;
+    this.map=this.createMap();
+  }
+
+
+  createMap(){
+    var arrMap=[];
+    for(let i=0;i<10;i++){
+    arrMap.push([]);
+      for(let j=0;j<10;j++){
+      arrMap[i].push(' ');
+      }
+    }
+    return arrMap;
+  }
+
+  launchShip(){
+    var rand = 9;
+    for(let i=0;i<=this.ship;i++){
+      var rowShip=Math.ceil(Math.random() * rand);
+      var colShip=Math.ceil(Math.random() * rand);
+      this.map[rowShip][colShip]='K';
+    }
+    return this.map;
+  }
+
+}
+
+const args = process.argv;
+var newBS= new BattleShip(2);
+console.log(newBS.launchShip());
+//console.log(args.createMap.arrMap[0,0]);
