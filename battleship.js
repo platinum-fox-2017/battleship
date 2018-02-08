a88c15b3028ac232ade1402271c625af9648a6c4
Herby Herado <herby,herado@gmail.com>
2018-02-08 16:45:35 +0700

    Release 0 & Release 1

--- battleship.js
+++ battleship.js
@@ -1,1 +1,26 @@
 // Your code here
+class Battleship{
+    constructor (){
+        this.boardSize = 10;
+        this.board = this.boards()
+    }
+
+    boards(){
+        let board = []
+        for (let i = 0; i < this.boardSize; i++){
+            board.push([])
+            for (let j = 0; j < this.boardSize; j++){
+                board[i].push(" ")
+            }
+        }
+        return board
+    }
+
+    randomEnemy () {
+
+    }
+
+}
+
+var ship = new Battleship()
+console.log(ship.boards())
