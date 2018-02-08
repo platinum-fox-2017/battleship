eec46ca89ee722a74785ba893eda209c53800a97
Devdots <devdots@Devdotss-MacBook-Pro.local>
2018-02-08 17:13:02 +0700

    release0

--- battleship.js
+++ battleship.js
@@ -1,1 +1,27 @@
 // Your code here
+class Battleship {
+  constructor(length) {
+    this.length = length
+
+
+  }
+
+  create_table() {
+    let board = []
+    for (var i = 0; i < this.length; i++) {
+      let row = []
+      for (var j = 0; j < this.length; j++) {
+        row.push('[ ]')
+      }
+      board.push(row)
+    }
+    return board
+  }
+
+
+}
+
+
+const ship = new Battleship(10)
+
+console.log(ship.create_table());
