66384b1b1ab8792c105f89d2918d3a16f2648dd1
agungcaproex <agung.caproex@gmail.com>
2018-02-08 21:57:30 +0700

    release 0

--- battleship.js
+++ battleship.js
@@ -1,1 +1,20 @@
-// Your code here
+class Battleship{
+  constructor(area){
+    this.area = area
+  }
+
+  createBoard(){
+    let size = []
+    for(let i=0; i<this.area; i++){
+      let row = []
+      for(let j=0; j<this.area; j++){
+        row.push('| |')
+      }
+      size.push(row)
+    }
+    return size
+  }
+}
+
+const warShip = new Battleship(10)
+console.log(warShip.createBoard())
