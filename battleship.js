9ca1109258579b2a8987982f1eca34ff83f341b7
Fachrizal Gita <Cecha@Cechas-MacBook-Pro.local>
2018-02-08 17:11:26 +0700

    release 0, board

--- battleship.js
+++ battleship.js
@@ -1,1 +1,26 @@
 // Your code here
+class Battleship {
+  constructor(length) {
+    this.length = length
+  }
+  print_board(length){
+    let result = []
+    for (let i = 0; i <= length; i++) {
+      let row = []
+      for (let j = 0; j <= length; j++) {
+        row.push('[ ]')
+      }
+      result.push(row)
+    }
+    for(let j = 1; j <=length; j++){
+      let dict = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      result[j][0] = '['+j+']'
+      result[0][j] = '['+dict[j-1]+']'
+    }
+    return result
+  }
+}
+
+let inputLength
+let kapalPerang = new Battleship(inputLength)
+console.log(kapalPerang.print_board(7));
